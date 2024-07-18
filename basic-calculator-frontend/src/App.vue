3d99e8aa83fbaea378940afd2846d7453ed8d197
federico-casu <federico.casu1101@gmail.com>
2024-07-18 19:53:51 +0200

    feat: added axios in frontend dependencies; added axios.get call to the backend to check if everything works ad expected

--- basic-calculator-frontend/src/App.vue
+++ basic-calculator-frontend/src/App.vue
@@ -1,30 +1,45 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    },
+    mounted() {
+      axios.get('http://127.0.0.1:8000/api/calculate', 
+        {
+          params: {
+            sum: this.sum,
+            sub: this.sub,
+            multiply: this.multiply,
+            divide: this.divide,
+          }
+        }
+      )
+      .then( res => {
+        this.results = res.data.results
+        console.log(res)
+        // console.log(this.results)
+      } )
+    }
+  }
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- html componente -->
+
+  
 </template>
 
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  /* stile del componente */
+
+  
 </style>
