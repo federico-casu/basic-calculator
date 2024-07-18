6726cbc5de02e4f54de6b068e8e9f4954890c060
federico-casu <federico.casu1101@gmail.com>
2024-07-18 21:14:48 +0200

    feat: (backend) added validateDivision method to manage edge cases

--- basic-calculator-backend/app/Http/Controllers/Api/ComputingController.php
+++ basic-calculator-backend/app/Http/Controllers/Api/ComputingController.php
@@ -19,12 +19,22 @@ class ComputingController extends Controller
             "sum" => $request->sum["n1"] + $request->sum["n2"], 
             "sub" => $request->sub["n1"] - $request->sub["n2"], 
             "multiply" => $request->multiply["n1"] * $request->multiply["n2"], 
-            "divide" => $request->divide["n1"] / $request->divide["n2"]
+            "divide" => validateDivision($request->divide["n1"], $request->divide["n2"])
         ];
 
         return response()->json([
             'success' => true,
             'results' => $results
         ]);
+    }
+}
+
+function validateDivision($n1, $n2) {
+    if ($n1 == 0 && $n2 == 0) {
+        return 'imp';
+    } elseif ($n2 == 0) {
+        return 'inf';
+    } else {
+        return $n1 / $n2;
     }
 }
--- basic-calculator-frontend/src/App.vue
+++ basic-calculator-frontend/src/App.vue
@@ -14,6 +14,7 @@
       }
     },
     methods: {
+      // method that sends operations and gets results
       calculate() {
         axios.get('http://127.0.0.1:8000/api/calculate', 
         {
